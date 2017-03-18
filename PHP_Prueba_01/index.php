59429e077e544580aab7b8f96a8c2fecc495e865
xabertum <mohatar@gmail.com>
2017-03-18 19:57:29 +0100

    first commit

--- PHP_Prueba_01/index.php
+++ PHP_Prueba_01/index.php
@@ -8,10 +8,33 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        
+        
+        <style>
+            
+            h2 {
+                color: red;
+            }
+            
+        </style>
+            
+        
     </head>
+    
+    
     <body>
+
+
         <?php
-        // put your code here
+        echo "My first PHP script!";
+
+
+        echo "<h2>PHP is Fun!</h2>";
+        echo "Hello world!<br>";
+        echo "I'm about to learn PHP!<br>";
+        echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+        
+        
         ?>
     </body>
 </html>
