238fd032a37eb249c57b1fb3bd248ada4f16d9e8
xabertum <mohatar@gmail.com>
2017-03-18 20:02:29 +0100

    minor commit

--- PHP_Prueba_01/index.php
+++ PHP_Prueba_01/index.php
@@ -8,33 +8,39 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
-        
-        
+
+
         <style>
-            
+
             h2 {
                 color: red;
             }
-            
-        </style>
-            
-        
-    </head>
-    
-    
-    <body>
 
+        </style>
+
+
+    </head>
+
+
+    <body>
 
         <?php
         echo "My first PHP script!";
-
 
         echo "<h2>PHP is Fun!</h2>";
         echo "Hello world!<br>";
         echo "I'm about to learn PHP!<br>";
         echo "This ", "string ", "was ", "made ", "with multiple parameters.";
-        
-        
+
+        $txt1 = "Learn PHP";
+        $txt2 = "W3Schools.com";
+        $x = 5;
+        $y = 4;
+
+        echo "<h2>$txt1</h2>";
+        echo "Study PHP at $txt2<br>";
+        echo $x + $y;
         ?>
+
     </body>
 </html>
