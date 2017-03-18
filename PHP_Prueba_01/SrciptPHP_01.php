7d61d563c3483fffce332b8eb541f2b9e2be87c6
xabertum <mohatar@gmail.com>
2017-03-18 22:36:45 +0100

    minor changes

--- PHP_Prueba_01/SrciptPHP_01.php
+++ PHP_Prueba_01/SrciptPHP_01.php
@@ -14,52 +14,6 @@ echo "Hello world!<br>";
 echo "I'm about to learn PHP!<br>";
 echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 
-$txt1 = "Learn PHP";
-$txt2 = "W3Schools.com";
-$x = 5;
-$y = 4;
-
-echo "<h2>$txt1</h2>";
-echo "Study PHP at $txt2<br>";
-echo $x + $y;
-
-echo "<h2>";
-
-echo str_word_count('Hola esto es un prueba maliciosa');
-
-echo "</h2>";
-
-function precio_con_iva(&$precio, $iva = 0.18) {
-
-    $precio *= (1 + $iva);
-}
-
-$precio = 10;
-precio_con_iva($precio);
-print "El precio con iva es " . $precio;
-
-echo "<br>";
-echo "<br>";
-
-$modulos = array("PR" => "programación", "BD" => "Base de datos");
-$modulos ["RC"] = "Radio Control";
-
-
-foreach ($modulos as $i => $i_value) {
-
-    echo "Posición: " . $i . ", valor: " . $i_value;
-    echo "<br>";
-}
-
-$modulos_value [] = str_word_count("Esto es una prueba");
-
-while ($modulos = each($modulos_value)) {
-
-    print $modulos[1] . " es " . $modulos_value[0] . "<br/>";
-}
-
-echo "<br>";
-echo "<br>";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $firstName = $_POST['firstname'];
